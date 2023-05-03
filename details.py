b67f2ca8c6875136c18e2e72e790164c68de63a3
KartikSharmaking <132456687+KartikSharmaking@users.noreply.github.com>
2023-05-03 17:58:19 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = 2363700
-api_hash = "527cf5174eigfkggigig3611bc69d7b7709"
-bot_token = "6180767554gjggkgkgifBsrDmKItUewgdKGAtGo"
+api_id = 5287680465
+api_hash = "13b2b99d1_e6a32136abc9a8cb"
+bot_token = "6075050967:AAG0biyOedtzMZhMH6UvcFKgEfK9MeOJXLE"
 
